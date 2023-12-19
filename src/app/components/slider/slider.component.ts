82c8b5fc8c3813b3d1594bd25db07258378116ea
berluk12@gmail.com <berluk12@gmail.com>
2023-12-19 19:28:11 +0100

    Favorites

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -8,8 +8,6 @@ import {Product} from "../../model/product";
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent {
-  images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
   @Input()
   products: Product[] | undefined = [];
 
